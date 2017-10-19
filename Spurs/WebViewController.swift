bd03f612a506650f92f7f76bdf861d08797d9697
nbv3698 <nbv3698@yahoo.com.tw>
2017-10-19 11:26:42 +0800

    20171019

---
+++ Spurs/WebViewController.swift
@@ -0,0 +1,41 @@
+//
+//  WebViewController.swift
+//  Spurs
+//
+//  Created by Len on 2017/10/18.
+//  Copyright © 2017年 gospursgo. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = URL(string: "http://www.nba.com/spurs/tim-duncan-career-retrospective") {
+            let request = URLRequest(url: url)
+            webView.loadRequest(request)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
